bcf44bce3d700ace569f2ac0d35c16bc879528ba
Shodai Inose <shodai.inose2@gmail.com>
2025-03-25 22:02:18 +0000

    update

---
+++ scripts/00_create_cohort/04_01_filter_continuous_enrollment.R
@@ -0,0 +1,56 @@
+# -------------------------------------
+# Script: 01_01_filter_continuous_enrollment.R
+# Author: Nick Williams
+# Purpose: Split enrollment periods into chunks per beneficiary
+# Notes:
+# -------------------------------------
+
+library(data.table)
+library(fst)
+library(arrow)
+library(lubridate)
+library(foreach)
+library(doFuture)
+library(dplyr)
+
+source("R/helpers.R")
+
+# Load washout dates
+washout <- load_data("msk_washout_opioid_requirements.fst")
+
+# Load all dates
+dates <- open_dedts()
+
+dates <- 
+  filter(dates, !is.na(BENE_ID)) |> 
+  select(BENE_ID, ENRLMT_START_DT, ENRLMT_END_DT) |>
+  inner_join(washout, by = "BENE_ID") |> 
+  collect()
+
+setDT(dates, key = "BENE_ID")
+
+dates <- dates[order(rleid(BENE_ID), ENRLMT_START_DT)]
+dates <- dates[!is.na(ENRLMT_START_DT) & !is.na(ENRLMT_END_DT)]
+dates <- dates[ENRLMT_START_DT <= exposure_end_dt]
+
+idx <- split(seq_len(nrow(dates)), list(dates$BENE_ID))
+tmp <- lapply(idx, \(x) dates[x])
+
+rm(idx, washout, dates)
+gc()
+
+# Define the function to split a list into chunks
+split_list_into_chunks <- function(lst, chunk_size) {
+  split(seq_along(lst), ceiling(seq_along(lst) / chunk_size))
+}
+
+chunks <- split_list_into_chunks(tmp, 1e5)
+
+# Save each chunk to a separate RDS file
+for (i in seq_along(chunks)) {
+  file_name <- paste0(
+    "/mnt/general-data/disability/everything-local-lmtp/tmp/enrollment_period_chunk_", 
+    i, ".rds"
+  )
+  saveRDS(tmp[chunks[[i]]], file = file_name)
+}
